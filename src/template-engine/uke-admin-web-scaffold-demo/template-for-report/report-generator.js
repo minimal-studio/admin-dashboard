466aa17070b9b7c2211d2b332f5e50b8c77a514a
ALEX <zhangxiangjie@zhangxiangjies-MacBook.local>
2018-10-09 10:38:03 +0800

    update

--- src/template-engine/uke-admin-web-scaffold-demo/template-for-report/report-generator.js
+++ src/template-engine/uke-admin-web-scaffold-demo/template-for-report/report-generator.js
@@ -2,36 +2,38 @@
  * 表格渲染模版
  */
 
- import React, {Component, PureComponent} from 'react';
+import React, {Component, PureComponent} from 'react';
 
- import ReportLayoutRender from './records-template';
+import ReportLayoutRender from './records-template';
 
- export function GeneralReportRender(Action, passProps = {}) {
-   return class C extends Action {
-     getActionBtn(item) {
-       const {actionBtnConfig} = this;
-       if(!actionBtnConfig) return '-';
-       return actionBtnConfig.map((config, idx) => {
-         const {text, action} = config;
-         return (
+export function GeneralReportRender(Action, passProps = {}) {
+  return class C extends Action {
+    getActionBtn(item) {
+      const {actionBtnConfig} = this;
+      if(!actionBtnConfig) return '-';
+      return actionBtnConfig.map((config, idx) => {
+        const {text, action} = config;
+        return (
           <span className="link-btn mr5" key={idx} onClick={e => {
             action(item);
-          }}>{text}</span>
-        )
-       })
-     }
-     render() {
-       return (
-         <ReportLayoutRender
-           keyMapper={this.keyMapper}
-           conditionOptions={this.conditionOptions}
-           needCount={this.needCount}
+          }}>
+            {text}
+          </span>
+        );
+      });
+    }
+    render() {
+      return (
+        <ReportLayoutRender
+          keyMapper={this.keyMapper}
+          conditionOptions={this.conditionOptions}
+          needCount={this.needCount}
 
-           {...passProps}
-           {...this.state}
-           {...this.props}
-           onQueryData={this.queryData.bind(this)}/>
-       )
-     }
-   }
- }
+          {...passProps}
+          {...this.state}
+          {...this.props}
+          onQueryData={this.queryData.bind(this)}/>
+      );
+    }
+  };
+}
