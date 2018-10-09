f78abe086061a66b1cb8c1771b2ac6207bd1f35c
SANGET <AlexZhang@Alexs-iMac.local>
2018-10-09 19:34:15 +0800

    update

--- src/pages/home-page/index.js
+++ src/pages/home-page/index.js
@@ -1,22 +1,22 @@
 import React, {Component} from 'react';
 import {TipPanel} from 'ukelli-ui';
 
-export default class HomePage extends Component {
-  render() {
-    return (
-      <div className="home-page">
-        <TipPanel title="Hi～" texts={[
-          '欢迎来到 uke admin web 的示例项目。',
-          '这是提供了基础管理后台的脚手架、命令生成工具，以及一套完整的开发流程。',
-        ]}/>
-        <TipPanel title="这是什么？" texts={[
-          '主要分了 Actions 和 Pages 两层。',
-          'Actions 专注于业务的逻辑，与远端服务器的数据交互的接口，以及继承系统提供的多个帮助函数。',
-          'Pages 专注于数据的渲染，Actions 与远端准备好数据，直接可以在 Pages 中体现。',
-          '两者的结合可以做到，系统的逻辑清晰，用户体验一致，快速开发。',
-        ]}/>
-        <TipPanel title="多谢"/>
-      </div>
-    )
-  }
-}
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <TipPanel title="Hi～" texts={[
+        '这里是 uke admin web 示例项目。',
+        '这是提供了基础管理后台的脚手架、命令生成工具，以及一套完整的开发流程。',
+      ]}/>
+      <TipPanel title="明细" texts={[
+        '主要分了 Services 和 Pages 两层。',
+        'Services 提供基础服务，提供与远端数据交互时，页面的 state 生命周期管理；提供所有表单表格注册和获取的方式；提供所有 api 接口的获取方式',
+        'Pages 专注于数据的渲染，定义页面具体的表现形式',
+        '两者的结合可以做到，系统的逻辑清晰，用户体验一致，快速开发。',
+      ]}/>
+      <TipPanel title="多谢"/>
+    </div>
+  );
+};
+
+export default HomePage;
