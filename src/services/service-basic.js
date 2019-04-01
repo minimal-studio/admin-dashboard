f93b907f8139dbfb016a6461938eebf6fa286f35
SANGET <AlexZhang@Alexs-iMac.local>
2019-04-01 17:38:00 +0800

    update services basic

--- src/services/service-basic.js
+++ src/services/service-basic.js
@@ -6,9 +6,13 @@
 
 import React, {Component, PureComponent} from 'react';
 
-import { Call, ToBasicUnitMoney, DebounceClaaass } from 'basic-helper';
+import {
+  Call, ToBasicUnitMoney, DebounceClaaass, IsEmail, IsPhoneNumber, HasValue,
+  EventEmitter
+} from 'basic-helper';
+import { getUrlParams } from 'uke-request';
 import ActionAgent from "uke-admin-web-scaffold/action-agent";
-import { Notify } from 'ukelli-ui';
+import { Notify, ShowModal, CloseModal } from 'ukelli-ui';
 
 import * as APIs from './apis';
 import * as paginHelper from '../utils/pagination-helper';
@@ -36,6 +40,14 @@ export default class Services extends ActionAgent {
   conditions = Conditions;
   /** 用于注册 forms */
   forms = Forms;
+  /** 原生的 getFormOptions 的方法，调用方式 this.getFromMapper(this.[conditions | forms][, ...]) */
+  getFromMapper = getFromMapper;
+  getFromMapperSync = getFromMapperSync;
+  //reportData 的默认数据
+  reportData = {
+    conditionData: {},
+    nextPagin: paginHelper.getDefPagin(),
+  }
   constructor(props) {
     super(props);
 
@@ -48,9 +60,89 @@ export default class Services extends ActionAgent {
       infoMapper: paginHelper.getPaginMapper(),
       pagingInfo: paginHelper.getDefPagin(),
       propsForTable: {
-        rowKey: record => record.ID
+        /** 统一设置 action 为固定右边的列 */
+        fixedRightKeys: ['action'],
+        /** 返回 record.ID 作为 Table 的 row 的 key，提高 Table 的渲染性能 */
+        rowKey: (record) => {
+          return record.ID;
+        }
       }
     };
+  }
+  saveRef = (ref) => (e) => this[ref] = e;
+  showResDesc(desc, message) {
+    if(desc == 'success') {
+      desc = '成功';
+    }
+    (this.toast || this.FormRef.toast).show(desc, message == 'success' ? 'success' : 'error');
+  }
+  /**
+   * 通过 url 的参数传递说明
+   * 解析目标 https://hostname?[ref1]=[value1]&&[...]
+   * 会解析为 {
+   *   [refs]: [value1]
+   *   ...
+   * } 并且作为 conditionOptions 或者 formOptions 的 defaultValue merge 到最终结果之中
+   * 
+   * @param {*} options 
+   */
+  paramsFilter = (val) => {
+    let res = decodeURIComponent(val);
+    switch (true) {
+    case HasValue(+val):
+      res = +val;
+      break;
+    case res.indexOf(',') !== -1:
+      res = res.split(',');
+      res.forEach((item, idx) => res[idx] = this.paramsFilter(item));
+      break;
+    }
+    return res;
+  }
+  conditionFilterFromUrl(options, needMerge = true, params) {
+    const conditionOptions = [...options];
+    if(!params) params = getUrlParams();
+    if (Object.keys(params).length > 0 && needMerge) {
+      conditionOptions.forEach((item, idx) => {
+        const { ref, refForS, refs } = item;
+        if(refForS && params[refForS]) {
+          conditionOptions[idx].defaultValueForS = this.paramsFilter(params[refForS]);
+        }
+        if(ref && params[ref]) {
+          conditionOptions[idx].defaultValue = this.paramsFilter(params[ref]);
+        }
+        if(refs && params[refs[0]]) {
+          conditionOptions[idx].range = [this.paramsFilter(params[refs[0]]), this.paramsFilter(params[refs[1]])];
+        }
+      });
+    }
+    
+    return conditionOptions;
+  }
+  async getConditionsSync() {
+    const conditionOptions = await getFromMapperSync(this.conditions, ...arguments);
+    return this.conditionFilterFromUrl(conditionOptions);
+  }
+  /**
+   * 获取查询条件
+   *
+   * @param {Array} options 需要获取的查询条件的名字们
+   * @param {Object} merge 需要合并到结果集的对象
+   * @returns {Array} 
+   * @memberof Services
+   */
+  getConditions() {
+    const conditionOptions = getFromMapper(this.conditions, ...arguments);
+    return this.conditionFilterFromUrl(conditionOptions);
+  }
+
+  async getFormsSync() {
+    const conditionOptions = await getFromMapperSync(this.forms, ...arguments);
+    return this.conditionFilterFromUrl(conditionOptions);
+  }
+  getForms() {
+    const conditionOptions = getFromMapper(this.forms, ...arguments);
+    return this.conditionFilterFromUrl(conditionOptions, false);
   }
   
   componentDidCatch(error, info) {
@@ -74,19 +166,6 @@ export default class Services extends ActionAgent {
     });
   }
 
-  async getConditionsSync() {
-    return await getFromMapperSync(this.conditions, ...arguments);
-  }
-  getConditions() {
-    return getFromMapper(this.conditions, ...arguments);
-  }
-
-  async getFormsSync() {
-    return await getFromMapperSync(this.forms, ...arguments);
-  }
-  getForms() {
-    return getFromMapper(this.forms, ...arguments);
-  }
   /**
    * 重写 ActionAgent 的 resStatus 接口
    * 调用顺序为接口 return 前
@@ -118,7 +197,6 @@ export default class Services extends ActionAgent {
     return checkRes.isPass;
   }
   reportDataFilter({ conditionData, nextPagin }) {
-    console.log(nextPagin)
     /**
      * 这里需要根据业务字段配置对应的
      */
